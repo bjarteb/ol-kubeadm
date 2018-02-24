63b7cf1232c8bc0fd61db8560915823d1fa29f3b
Bjarte Brandt <bjarte.brandt@tv2.no>
2018-02-24 09:11:00 +0100

    Kubernetes cluster on Oracle Linux.

---
+++ provisioning/bootstrap-master.sh
@@ -0,0 +1,19 @@
+yum install -y yum-utils
+yum-config-manager --enable ol7_addons
+yum-config-manager --enable ol7_preview
+
+/usr/sbin/setenforce 0
+yum install -y docker-engine
+systemctl enable docker
+systemctl start docker
+yum install -y kubeadm
+iptables -P FORWARD ACCEPT
+/sbin/sysctl -p /etc/sysctl.d/k8s.conf
+
+export KUBE_REPO_PREFIX=container-registry.oracle.com/kubernetes
+cat /vagrant/my_password.txt | docker login --username ${USERNAME} --password-stdin container-registry.oracle.com
+
+kubeadm-setup.sh up --apiserver-advertise-address 10.0.18.10
+
+kubeadm token list | awk 'NR==2 { print $1; }' > /vagrant/token
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > /vagrant/ca-cert-hash
---
+++ provisioning/bootstrap-worker.sh
@@ -0,0 +1,17 @@
+yum install -y yum-utils
+yum-config-manager --enable ol7_addons
+yum-config-manager --enable ol7_preview
+
+/usr/sbin/setenforce 0
+yum install -y docker-engine
+systemctl enable docker
+systemctl start docker
+yum install -y kubeadm
+iptables -P FORWARD ACCEPT
+/sbin/sysctl -p /etc/sysctl.d/k8s.conf
+
+cat /vagrant/my_password.txt | docker login --username ${USERNAME} --password-stdin container-registry.oracle.com
+# join cluster
+TOKEN=$(cat /vagrant/token)
+CA_CERT_HASH=$(cat /vagrant/ca-cert-hash)
+export KUBE_REPO_PREFIX=container-registry.oracle.com/kubernetes& kubeadm-setup.sh join --token $TOKEN 10.0.18.10:6443 --discovery-token-ca-cert-hash sha256:${CA_CERT_HASH}
