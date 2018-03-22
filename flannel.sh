040fdf9418ceb9d165b03500841c091235e7413f
Bjarte Brandt <bjarte.brandt@tv2.no>
2018-03-22 07:47:20 +0100

    The sed expression does not work on macOS.

--- flannel.sh
+++ flannel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export KUBECONFIG=/Users/BJBRA/vagrant/ol-kubeadm/admin.conf
-curl -sL https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml |sed "/kube-subnet-mgr/a\        - --iface=eth1" | kubectl replace -f -
+export KUBECONFIG=$(pwd)/admin.conf
+curl -sL https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml |awk '1;/kube-subnet-mgr/{ print "        - --iface=eth1";}' | kubectl replace -f -
